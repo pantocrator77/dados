ef177457c75672e3cc3256cc3be4f182c2df4c0b
pantok <lorenzo.vecchia@gmail.com>
2022-05-25 15:19:34 +0200

    updated

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
